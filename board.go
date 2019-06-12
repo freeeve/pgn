9dd0095bf0ae2c9ba0be93e4645aefa656c11e78
andWhatShouldISay <zdd-2000@yandex.ru>
2019-06-12 22:08:19 +0300

    fixed the bug with ambigous en passant

--- board.go
+++ board.go
@@ -618,24 +618,7 @@ func (b Board) findAttackingPawn(pos Position, color Color, check bool) (Positio
 	count := 0
 	switch color {
 	case White:
-		// special en-passant case
-		if b.lastMove.To.GetFile() == pos.GetFile() &&
-			b.lastMove.To.GetRank() == pos.GetRank()-1 &&
-			b.lastMove.From.GetRank() == pos.GetRank()+1 &&
-			b.GetPiece(PositionFromFileRank(pos.GetFile(), pos.GetRank()-1)) == BlackPawn {
-			testPos := PositionFromFileRank(pos.GetFile()+1, pos.GetRank()-1)
-			if b.GetPiece(testPos) == WhitePawn &&
-				(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
-				retPos = testPos
-				count++
-			}
-			testPos = PositionFromFileRank(pos.GetFile()-1, pos.GetRank()-1)
-			if b.GetPiece(testPos) == WhitePawn &&
-				(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
-				retPos = testPos
-				count++
-			}
-		}
+
 		testPos := PositionFromFileRank(pos.GetFile()+1, pos.GetRank()-1)
 		if b.GetPiece(testPos) == WhitePawn &&
 			(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
@@ -649,26 +632,7 @@ func (b Board) findAttackingPawn(pos Position, color Color, check bool) (Positio
 			count++
 		}
 	case Black:
-		// special en-passant case
-		if b.lastMove.To.GetFile() == pos.GetFile() &&
-			b.lastMove.To.GetRank() == pos.GetRank()+1 &&
-			b.lastMove.From.GetRank() == pos.GetRank()-1 &&
-			b.GetPiece(PositionFromFileRank(pos.GetFile(), pos.GetRank()+1)) == WhitePawn {
-			testPos := PositionFromFileRank(pos.GetFile()+1, pos.GetRank()+1)
-			if b.GetPiece(testPos) == BlackPawn &&
-				(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
-				retPos = testPos
-				count++
-				break
-			}
-			testPos = PositionFromFileRank(pos.GetFile()-1, pos.GetRank()+1)
-			if b.GetPiece(testPos) == BlackPawn &&
-				(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
-				retPos = testPos
-				count++
-				break
-			}
-		}
+
 		testPos := PositionFromFileRank(pos.GetFile()+1, pos.GetRank()+1)
 		if b.GetPiece(testPos) == BlackPawn &&
 			(!check || !b.moveIntoCheck(Move{testPos, pos, NoPiece}, color)) {
@@ -696,40 +660,14 @@ func (b Board) findAttackingPawnFromFile(pos Position, color Color, file File) (
 	retPos := NoPosition
 	count := 0
 	if color == White {
-		// special en-passant case
-		if b.lastMove.To.GetFile() == pos.GetFile() &&
-			b.lastMove.To.GetRank() == pos.GetRank()-1 &&
-			b.lastMove.From.GetRank() == pos.GetRank()+1 &&
-			b.GetPiece(PositionFromFileRank(pos.GetFile(), pos.GetRank()-1)) == BlackPawn {
-			if b.GetPiece(PositionFromFileRank(file, pos.GetRank()-1)) == WhitePawn {
-				retPos = PositionFromFileRank(file, pos.GetRank()-1)
-				count++
-			}
-		}
 		if b.GetPiece(PositionFromFileRank(file, pos.GetRank()-1)) == WhitePawn {
 			retPos = PositionFromFileRank(file, pos.GetRank()-1)
 			count++
 		}
-		if count == 2 {
-			count = 1
-		}
 	} else {
-		// special en-passant case
-		if b.lastMove.To.GetFile() == pos.GetFile() &&
-			b.lastMove.To.GetRank() == pos.GetRank()+1 &&
-			b.lastMove.From.GetRank() == pos.GetRank()-1 &&
-			b.GetPiece(PositionFromFileRank(pos.GetFile(), pos.GetRank()+1)) == WhitePawn {
-			if b.GetPiece(PositionFromFileRank(file, pos.GetRank()+1)) == BlackPawn {
-				retPos = PositionFromFileRank(file, pos.GetRank()+1)
-				count++
-			}
-		}
 		if b.GetPiece(PositionFromFileRank(file, pos.GetRank()+1)) == BlackPawn {
 			retPos = PositionFromFileRank(file, pos.GetRank()+1)
 			count++
-		}
-		if count == 2 {
-			count = 1
 		}
 	}
 	if count > 1 {
